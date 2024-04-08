271f93bc74eac7035a08f4559551245b2de245b4
chanhee-lee <clee3243@gmail.com>
2024-04-08 17:57:37 +0900

    bufferline - move tabs

--- nvim/.config/nvim/lua/plugins/editor/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/editor/bufferline.lua
@@ -3,8 +3,25 @@ return
 		"akinsho/bufferline.nvim",
 		version = "*",
 		dependencies = "nvim-tree/nvim-web-devicons",
+		-- keys = {
+		-- 	{ "<leader>bmn", ":BufferLineMoveNext<CR>", desc = "[b]uffer [m]ove to [n]ext buffer" },
+		-- 	{ "<leader>bmp", ":BufferLineMovePrev<CR>", desc = "[b]uffer [m]ove to [p]revious buffer" },
+		-- },
 		config = function()
 			vim.opt.termguicolors = true
-			require("bufferline").setup({})
+			require("bufferline").setup({
+				options = { -- Add Pinned icon
+					groups = {
+						items = {
+							require('bufferline.groups').builtin.pinned:with({ icon = "" })
+							-- You can add other items here
+						}
+					}
+				}
+			})
+			-- key mapping
+			vim.api.nvim_set_keymap('n', '<leader>bn', ':BufferLineMoveNext<CR>', { noremap = true, silent = true })
+			vim.api.nvim_set_keymap('n', '<leader>bp', ':BufferLineMovePrev<CR>', { noremap = true, silent = true })
+
 		end,
 	}
