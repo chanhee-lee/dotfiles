7629e44f14599438948184b0b0ebe4d5dcf5803f
chanhee-lee <clee3243@gmail.com>
2024-08-07 14:53:11 +0900

    moving and updating utf-8 encoding for nvim initi.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+require("core.keymaps")
+require("core.options")
+require("core.lazy")
+require("core.yank-highlight")
+-- Set file encoding to UTF-8
+vim.o.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
+vim.o.fileencodings = "utf-8"
